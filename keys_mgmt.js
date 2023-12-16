ad5f491b5b89c89681f2849e506520d79522edd0
florian <florian.carriere@gmail.com>
2023-12-16 19:45:38 +0100

    test secret management

--- keys_mgmt.js
+++ keys_mgmt.js
@@ -2,33 +2,34 @@
 // If you need more information about configurations or implementing the sample code, visit the AWS docs:
 // https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
 
-import {
-    SecretsManagerClient,
-    GetSecretValueCommand,
-  } from "@aws-sdk/client-secrets-manager";
-  
-  const secret_name = "Strava_keys";
-  
-  const client = new SecretsManagerClient({
-    region: "eu-west-3",
+// Importer le module aws-sdk pour accéder au secret manager
+const AWS = require('aws-sdk');
+
+// Créer un client pour le secret manager, en spécifiant la région
+const client = new AWS.SecretsManager({
+    region: 'eu-west-3' // La région où se trouve votre secret
   });
+
+// Le nom de votre secret dans le secret manager
+const secretName = 'strava_keys';
+
+// Appeler la méthode getSecretValue avec le nom du secret
+client.getSecretValue({ SecretId: secretName }, function(err, data) {
+    if (err) {
+      // Gérer les erreurs
+      console.error('Erreur lors de la récupération du secret', err);
+    } else {
+      // Récupérer les secrets depuis la chaîne SecretString
+      // Vous pouvez aussi utiliser le binaire SecretBinary selon le format de votre secret
+      const secrets = JSON.parse(data.SecretString);
   
-  let response;
-  
-  try {
-    response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secret_name,
-        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-      })
-    );
-  } catch (error) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    throw error;
-  }
-  
-  const secret = response.SecretString;
-  
-  // Your code goes here
-  console.log(`Réponse secret AWS : `+secret);
+      // Extraire les informations utiles
+      const client_id = secrets.client_id;
+      const client_secret = secrets.client_secret;
+      const refresh_token = secrets.refresh_token;
+    }
+});  
+
+console.log(`client_id = `+client_id);
+console.log(`client_secret = `+client_secret);
+console.log(`refresh_token = `+refresh_token);
