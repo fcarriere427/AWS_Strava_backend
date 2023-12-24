909c12c8cc9e985ec362bee9cf623403de5cb30a
florian <florian.carriere@gmail.com>
2023-12-24 01:10:03 +0100

    cleaning logs, OK for secrets :-)

--- routes/secret.js
+++ routes/secret.js
@@ -5,17 +5,9 @@ module.exports = {
     config: (router) => {
         router
             .get("/", (req, res) => {
-console.log('1er get');
               get_secret.getStravaSecret()
               .then((secrets) => {
                 console.log("... récupération des secrets : OK");
-                // Extraire les informations utiles --> à reprendre dans la fonction principale
-                const client_id = secrets.client_id;
-                const client_secret = secrets.client_secret;
-                const refresh_token = secrets.refresh_token;
-                console.log(`client_id = `+client_id);
-                console.log(`client_secret = `+client_secret);
-                console.log(`refresh_token = `+refresh_token);                
                 // renvoi du json 
                 res.setHeader('content-type', 'application/json');
                 res.status(200).send(secrets);
@@ -26,3 +18,12 @@ console.log('1er get');
     },
 };
 
+
+
+// Extraire les informations utiles --> à reprendre dans la fonction principale
+// const client_id = secrets.client_id;
+// const client_secret = secrets.client_secret;
+// const refresh_token = secrets.refresh_token;
+// console.log(`client_id = `+client_id);
+// console.log(`client_secret = `+client_secret);
+// console.log(`refresh_token = `+refresh_token);                
