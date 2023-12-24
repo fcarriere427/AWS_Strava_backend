f1ef6356bc73faa6dcfe032b67b8e9e350279402
florian <florian.carriere@gmail.com>
2023-12-24 01:07:14 +0100

    insert promise in get_secret

--- get_secret.js
+++ get_secret.js
@@ -2,24 +2,26 @@
 const AWS = require('aws-sdk');
 
 
-function getStravaSecret(){    
+function getStravaSecret(){   
+  return new Promise((resolve, reject) => {
     // Créer un client pour le secret manager, en spécifiant la région
-  const client = new AWS.SecretsManager({
-    region: 'eu-west-3' // La région où se trouve votre secret
-  });
-  // Nom de votre secret dans le secret manager
-  const secretName = 'strava_keys';
-  // Appeler la méthode getSecretValue avec le nom du secret
-  client.getSecretValue({ SecretId: secretName }, function(err, data) {
-    if (err) {
-      // Gérer les erreurs
-      console.error('Erreur lors de la récupération du secret', err);
-    } else {
-      // Récupérer les secrets depuis la chaîne SecretString
-      const secrets = JSON.parse(data.SecretString);
-      return(secrets);
-    }
-  });  
+    const client = new AWS.SecretsManager({
+      region: 'eu-west-3' // La région où se trouve votre secret
+    });
+    // Nom de votre secret dans le secret manager
+    const secretName = 'strava_keys';
+    // Appeler la méthode getSecretValue avec le nom du secret
+    client.getSecretValue({ SecretId: secretName }, function(err, data) {
+      if (err) {
+        // Gérer les erreurs
+        console.error('Erreur lors de la récupération du secret', err);
+      } else {
+        // Récupérer les secrets depuis la chaîne SecretString
+        const secrets = JSON.parse(data.SecretString);
+        resolve(secrets)
+      }
+    });
+  }) 
 }
 
 module.exports={
