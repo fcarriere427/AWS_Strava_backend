d6a665a36f2ddf687463552fc047299e4cd0ad48
florian <florian.carriere@gmail.com>
2023-12-24 16:04:32 +0100

    test

--- routes/readDB.js
+++ routes/readDB.js
@@ -1,14 +1,39 @@
 // Importer le module express
 import express from "express";
 
+// Importer les modules nécessaires à l'accès à DynamoDB
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { QueryCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
 // Créer un routeur express
 const readDBRouter = express.Router();
 
 // Envoyer la réponse (attention "/" car le reste de l'URL est géré par le routeur "routes.js" à la racine)
 readDBRouter.get("/", (req, res) => {
   console.log('lecture de la DB');
-  res.send("FAKE NEWS FOR NOW");
+  callDB()
+  .then((response) => {
+    res.send(response);
+    }
+  )
+
 });
+
+// Exécuter la requête et afficher la réponse
+async function callDB() {
+  // Créer un client DynamoDB
+  const client = new DynamoDBClient({});
+  // Créer un client document DynamoDB
+  const docClient = DynamoDBDocumentClient.from(client);
+  // Définir les paramètres de la requête
+  const params = {
+    TableName: "StravaDB", // Le nom de la table DynamoDB
+  };
+  const command = new QueryCommand(params);
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
+}
 
 // Exporter le routeur
 export default readDBRouter;
