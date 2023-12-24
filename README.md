0db2ba11ea7a46226d13e2c702a3c6dad0a71698
florian <florian.carriere@gmail.com>
2023-12-24 13:15:51 +0100

    new test

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-ES6 or CommonJS --> CommonJS, so no "type : module" in package.json
 
 Install
 - npm init
 - npm install express
 - npm install @aws-sdk/client-secrets-manager
-- 
+- npm install @aws-sdk/client-dynamodb
+
+
+Remarques : 
+- CommonJS (no ES6), so no "type : module" in package.json, and "require" instead of "import"
---
+++ routes/last_activity.js
@@ -0,0 +1,30 @@
+// API endpoint
+// First try to access Dynamo DB "StravaDB"
+
+// Importer le module custom d'appel à la DB 
+import { callDB } from "./callDB";
+
+module.exports = {
+    path: "/db",
+    config: (router) => {
+        router
+            .get("/", (req, res) => {
+              console.log('on va appeler la DB')
+              callDB.callDB()
+              .then((response) => {
+                res.setHeader('content-type', 'application/json');
+                res.status(200).send(response);
+              })
+            })
+            .post("/", (req, res) => res.send("No POST here!"));
+        return router;
+    },
+};
+
+// // Exécuter la requête et afficher la réponse
+// export const main = async () => {
+//   const command = new QueryCommand(params);
+//   const response = await docClient.send(command);
+//   console.log(response);
+//   return response;
+// };
