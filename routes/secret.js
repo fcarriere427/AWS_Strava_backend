a124f7808ed672e2afde2c1059af3515a3b72121
florian <florian.carriere@gmail.com>
2023-12-24 10:08:16 +0100

    test

--- routes/secret.js
+++ routes/secret.js
@@ -20,12 +20,13 @@ module.exports = {
 
  function getStravaSecret(){   
   return new Promise((resolve, reject) => {
-    const client = new SecretsManagerClient();  
+    ////const client = new SecretsManagerClient();  
     // Nom de votre secret dans le secret manager
     const secretName = 'strava_keys';
     // Appeler la méthode getSecretValue avec le nom du secret
-    client.getSecretValue({ SecretId: secretName }, function(err, data) {
-      if (err) {
+    //client.getSecretValue({ SecretId: secretName }, function(err, data) {
+    getSecretValue({ SecretId: secretName }, function(err, data) {
+        if (err) {
         // Gérer les erreurs
         console.error('Erreur lors de la récupération du secret', err);
       } else {
@@ -37,21 +38,25 @@ module.exports = {
   }) 
 }
 
-// module.exports={
-//   getStravaSecret
-// }
 
-// // Extraire les informations utiles --> à reprendre dans la fonction principale
-// const client_id = secrets.client_id;
-// const client_secret = secrets.client_secret;
-// const refresh_token = secrets.refresh_token;   
- 
-//const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager"); // CommonJS import
+function getSecretValue = async (secretName = "SECRET_NAME") => {
+  const client = new SecretsManagerClient();
+  const response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secretName,
+    }),
+  );
+  console.log(response);
+  if (response.SecretString) {
+    return response.SecretString;
+  }
 
-// Créer un client pour le secret manager, en spécifiant la région
-    // const client = new AWS.SecretsManager({
-    //   region: 'eu-west-3' // La région où se trouve votre secret
-    // });
+  if (response.SecretBinary) {
+    return response.SecretBinary;
+  }
+};
+
+
 
 
 
