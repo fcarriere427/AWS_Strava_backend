d928fcb8cb2a5a00224922e5f6fe5bd2df28da37
florian <florian.carriere@gmail.com>
2023-12-27 01:41:30 +0100

    j'en ai ras le bol des default export...

--- routes/getLastStravaActivity.js
+++ routes/getLastStravaActivity.js
@@ -2,7 +2,7 @@
 import express from "express";
 // Importer la librairie Strava perso
 //const strava = require('../stravaLib.js');
-import * as stravaLib from "../stravaLib.js";
+import getLastActivity from "../stravaLib";
 
 // Créer un routeur express
 const getLastStravaActivityRouter = express.Router();
@@ -10,7 +10,7 @@ const getLastStravaActivityRouter = express.Router();
 // Envoyer la réponse (attention "/" car le reste de l'URL est géré par le routeur "routes.js" à la racine)
 getLastStravaActivityRouter.get("/", (req, res) => {
   console.log('récupération de la dernière activité Strava');
-  stravaLib.getLastActivity()
+  getLastActivity()
   .then((response) => {
     res.send(response);
     }
