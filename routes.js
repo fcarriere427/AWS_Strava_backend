991dfd1bc7da890a578b2e6cc6dfa274135316a3
florian <florian.carriere@gmail.com>
2023-12-30 18:31:51 +0100

    test add strava activity in db

--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 
-import addItemDBRouter from './routes/addItemDB.js'
+import addItem from './routes/addItemDB.js'
 import getItemDBRouter from './routes/getItemDB.js'
 import getLastStravaActivityRouter from './routes/getLastStravaActivity.js'
 
 function routes(app) {
   app.use("/getLastActivity", getLastStravaActivityRouter);
-  app.use("/addItemDB", addItemDBRouter);
+  app.use("/addItemDB", addItem);
   app.use("/getItemDB", getItemDBRouter);
 };
 
--- routes/addItemDB.js
+++ routes/addItemDB.js
@@ -2,16 +2,18 @@
 import express from "express";
 // Importer la librairie Strava perso
 import addItemDB from "../dbLib.js";
+import getLastActivity from "../stravaLib.js";
 
 // Créer un routeur express
 const addItemDBRouter = express.Router();
 
 // Envoyer la réponse (attention "/" car le reste de l'URL est géré par le routeur "routes.js" à la racine)
-export default addItemDBRouter.get("/", (req, res) => {
-  console.log('*** addItemDB.js : appel de addItemDB in dbLib.js');
-  addItemDB()
-  .then((response) => {
+export default async function addItem() {
+  addItemDBRouter.get("/", async (req, res) => {
+    console.log('*** addItemDB.js : appel de addItemDB in dbLib.js');
+    console.log('*** addItemDB.js : appel de addItemDB in dbLib.js');
+    var activity = await getLastActivity();
+    var response = await addItemDB(activity);
     res.send(response);
-    }
-  )
-});
+  });
+};
