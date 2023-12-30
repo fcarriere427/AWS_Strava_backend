2d9a290858f888bfe3e0b6af1bf58ba2d2d8cdd3
florian <florian.carriere@gmail.com>
2023-12-30 22:04:58 +0100

    correct

--- dbLib.js
+++ dbLib.js
@@ -27,33 +27,33 @@ export default async function addItemDB(item) {
 
 // TMP : pour l'instant, ne fait que lire l'élément [0]
 export async function getItemDB() {
-  // // Spécifier la région
-  // const config = {region: 'eu-west-3'};
-  // // Créer un client DynamoDB
-  // const client = new DynamoDBClient(config);
-  // // Créer un client document DynamoDB
-  // const docClient = DynamoDBDocumentClient.from(client);
-  // // Définir les paramètres de la requête
-  // const params = {
-  //   TableName: "StravaDB", // Le nom de la table DynamoDB
-  //   Key:{ID: 0},
-  //   ConsistentRead: true,
-  // };
-  // const command = new GetCommand(params);
-  // const response = await docClient.send(command);
-  // return response;
-
-  console.log('*** getItemDB in dbLib.js')
-  const getCommand = new GetCommand({
-    TableName: "StravaDB",
-    Key: {
-      ID: 0
-    },
+    console.log('*** getItemDB in dbLib.js')
+  // Spécifier la région
+  const config = {region: 'eu-west-3'};
+  // Créer un client DynamoDB
+  const client = new DynamoDBClient(config);
+  // Créer un client document DynamoDB
+  const docClient = DynamoDBDocumentClient.from(client);
+  // Définir les paramètres de la requête
+  const params = {
+    TableName: "StravaDB", // Le nom de la table DynamoDB
+    Key:{ID: 0},
     ConsistentRead: true,
-  });
-  const getResponse = await docClient.send(getCommand);
-  console.log(`Got the movie: ${JSON.stringify(getResponse.Item)}`);
-  return 0;
+  };
+  const command = new GetCommand(params);
+  const response = await docClient.send(command);
+  return response;
+
+  // const getCommand = new GetCommand({
+  //   TableName: "StravaDB",
+  //   Key: {
+  //     ID: 0
+  //   },
+  //   ConsistentRead: true,
+  // });
+  // const getResponse = await docClient.send(getCommand);
+  // console.log(`Got the movie: ${JSON.stringify(getResponse.Item)}`);
+  // return 0;
 }
 
 
