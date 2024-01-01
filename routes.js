3b6a4ef454eddf57afd8f3d7b81f72080e9fba9c
florian <florian.carriere@gmail.com>
2024-01-01 23:11:46 +0100

    typo

--- routes.js
+++ routes.js
@@ -3,12 +3,12 @@ import addItem_DB_Router from './routes/addItem_DB.js'
 import getItem_DB_Router from './routes/getItem_DB.js'
 // routers to Strava API
 import getStats_Strava_Router from './routes/getStats_Strava.js'
-import getAllActivities_Strava_Router from './routes/getAllActivities_Strava.js'
+import getAllActivities_Strava_Router from './routes/getActivities_Strava.js'
 
 function routes(app) {
   // routes to Strava API
   app.use("/strava/getStats", getStats_Strava_Router);
-  app.use("/strava/getAllActivities", getAllActivities_Strava_Router);
+  app.use("/strava/getAllActivities", getActivities_Strava_Router);
   // routes to AWS DB
   app.use("/db/addItem", addItem_DB_Router);
   app.use("/db/getItem", getItem_DB_Router);
