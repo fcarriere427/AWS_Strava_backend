b0fd43651aa0a39b49976861eac97a30adfdbd9a
florian <florian.carriere@gmail.com>
2024-01-01 13:06:18 +0100

    add log

--- dbLib.js
+++ dbLib.js
@@ -14,6 +14,7 @@ export default async function addItemDB(activity, tableName) {
   // Créer un client document DynamoDB
   const docClient = DynamoDBDocumentClient.from(client);
   // Définir les paramètres de la requête
+  console.log('activity.id = ' + activity.id);
   var numID = Math.floor(Math.random()*100);
   console.log('ID for this new Activity = ' + numID);
   const params = {
