9255e0f147ea486587225a2c73ba75ee3fdfe117
florian <florian.carriere@gmail.com>
2024-01-01 19:42:53 +0100

    log

--- routes/getAllActivities_Strava.js
+++ routes/getAllActivities_Strava.js
@@ -16,7 +16,10 @@ export default getAllActivities_Strava_Router
     console.log('*** getAllActivities_Strava.js');
     console.log('Appel de getAllActivities');
     var response = await getAllActivities(id_athlete);
-    console.log('response = ' + JSON.stringify(response)); // renvoie un JSON avec les metadata en tête
+    console.log('response = ' + response);
+    response = JSON.parse(response);
+    console.log('response after JSON.parse() = ' + response);
+    console.log('response stringified after JSON.parse() = ' + JSON.stringify(response));
     console.log('Réponse envoyée');
-    res.send(JSON.parse(response)); //JSON.parse() car getAllActivities renvoie un entier (le nb d'activités récupérées)
+    res.send(response);
   });
