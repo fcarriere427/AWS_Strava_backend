6705caa05ce1b350bc274de18d82bdd0a7164c0a
florian <florian.carriere@gmail.com>
2024-01-01 17:36:07 +0100

    ok ?

--- routes/getStravaStats.js
+++ routes/getStravaStats.js
@@ -22,7 +22,6 @@ export default getStravaStatsRouter
     console.log('*** getAllStravaActivities.js');
     var response = await getStravaStats(id_athlete);
     console.log('Réponse envoyée');
-    // console.log('Nb total d\'activités = ' + response.all_ride_totals.count + response.all_run_totals.count + response.all_swin_totals.count);
-    console.log('test = ' + response.all_ride_totals.count);
+    console.log('Nb total d\'activités = ' + response.all_ride_totals.count + response.all_run_totals.count + response.all_swim_totals.count);
     res.send(response);
   });
