9a5d3893c051db48bf2332977796016a057141a2
florian <florian.carriere@gmail.com>
2024-01-06 23:51:07 +0100

    test

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ export const main = async () => {
           PutRequest: {
             Item: {
               ID: { N : "2" },
-              Contenu: {M : `{"prenom": '+'"bob", "nom": '+'"sinclar"}`},
+              Contenu: {M : `{key:value}`},
               },
           },
         },
