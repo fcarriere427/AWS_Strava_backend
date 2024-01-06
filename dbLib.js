79a9f82d24610a8fb21a193a53337efa3b67325a
florian <florian.carriere@gmail.com>
2024-01-06 22:26:47 +0100

    test

--- dbLib.js
+++ dbLib.js
@@ -21,8 +21,9 @@ export async function addBatchItem(input_batch, tableName) {
   console.log(`nbActivities = ${nbActivities}`);
   //console.log("JSON.stringify(input_batch) = " + JSON.stringify(input_batch));
   //console.log("type of JSON.stringify(input_batch) = " + typeof(JSON.stringify(input_batch)));
-  console.log("JSON.parse(input_batch) = " + JSON.parse(input_batch));
-  console.log("type of JSON.parse(input_batch) = " + typeof(JSON.parse(input_batch)));
+  let chaineJSON = input_batch.replace(/(\w+)/g, "\"$1\""); // "[\"a\",\"b\",\"c\",\"d\"]"
+  console.log("JSON.parse(chaineJSON) = " + JSON.parse(chaineJSON));
+  console.log("type of JSON.parse(chaineJSON) = " + typeof(JSON.parse(chaineJSON)));
   // console.log("JSON.stringify(input_batch[0]) = " + JSON.stringify(input_batch[0]));
   // console.log("type of JSON.stringify(input_batch[0]) = " + typeof(JSON.stringify(input_batch[0])));
   
