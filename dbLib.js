f5ac0f768c3e7000d8987d258c0f8d0ce1729152
florian <florian.carriere@gmail.com>
2024-01-06 18:24:25 +0100

    test

--- dbLib.js
+++ dbLib.js
@@ -65,7 +65,7 @@ export async function addBatchItem(input_batch, tableName) {
   }
   const input = {
     "RequestItems": {
-      `${tableName}`: batch
+      "stravaDB": batch
     }
   }
   const command = new BatchWriteItemCommand(input);
